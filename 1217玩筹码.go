eca620996e98790d5159d6fb201340434100df02
rachaelyychen <248542890@qq.com>
2021-03-19 09:36:11 +0800

    easy day 12

--- 1217玩筹码.go
+++ 1217玩筹码.go
@@ -1,5 +1,16 @@
 package main
 
-
-
-
+func minCostToMoveChips(position []int) int {
+	a, b := 0, 0
+	for i := range position {
+		if position[i] % 2 == 0 {
+			a += 1
+		} else {
+			b += 1
+		}
+	}
+	if a > b {
+		return b
+	}
+	return a
+}
